c3f3d9967dda37e4d158c66c46f871dd892d5e28
Storm00212 <paulmuyalikhams@gmail.com>
2026-01-21 23:28:57 +0300

    changes

--- electron-app/src/main.js
+++ electron-app/src/main.js
@@ -1,6 +1,5 @@
-const electron = require('electron');
-console.log('electron keys:', Object.keys(electron));
-const { app, BrowserWindow, ipcMain } = electron;
+console.log('global.app:', global.app);
+const { app, BrowserWindow, ipcMain } = require('electron');
 console.log('app:', app);
 const path = require('path');
 const { spawn } = require('child_process');
