371121a5eef662f2b9a4bfa8d25f5e2cdef6a68a
Storm00212 <paulmuyalikhams@gmail.com>
2026-01-22 00:08:38 +0300

    fixes

--- electron-app/src/main.js
+++ electron-app/src/main.js
@@ -41,10 +41,12 @@ function createWindow() {
 
 setTimeout(() => {
   console.log('global.electron:', global.electron);
-  const electron = require('electron');
-  console.log('electron:', electron);
-  const { app, BrowserWindow, ipcMain } = electron;
+  const app = require('app');
   console.log('app:', app);
+  const BrowserWindow = require('browser-window');
+  console.log('BrowserWindow:', BrowserWindow);
+  const ipcMain = require('ipc-main');
+  console.log('ipcMain:', ipcMain);
   console.log('delayed app:', app);
   app.on('ready', async () => {
     console.log('App ready event fired');
