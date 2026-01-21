52c7f57d962f7dc0633eebf2e7cc5eed7e4fa771
Storm00212 <paulmuyalikhams@gmail.com>
2026-01-21 23:30:13 +0300

    changed path

--- electron-app/src/main.js
+++ electron-app/src/main.js
@@ -20,7 +20,7 @@ function createWindow() {
     }
   });
 
-  mainWindow.loadFile(path.join(__dirname, 'index.html'));
+  mainWindow.loadFile(path.join(__dirname, '../dist/index.html'));
 }
 
 app.on('ready', async () => {
